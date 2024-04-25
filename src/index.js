44e07151bdc53d105b02b353eb9531b9dd2b2fc7
Deep Duggal <7544238+deepduggal@users.noreply.github.com>
2024-04-25 19:40:01 -0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+// import _ from 'lodash';
+import './styles.css';
+
+document.getElementById('content').innerText = 'Hello, World!';
+
+// const newText = _.camelCase('hello world');
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'build.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  mode: 'development',
+  plugins: [new MiniCssExtractPlugin()],
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          { loader: 'style-loader' },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+            },
+          },
+          // { loader: 'sass-loader' },
+        ],
+      },
+    ],
+  },
+};
